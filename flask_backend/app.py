85df8c55ddb5e9ca97c49a1303f8e0c3ed0d0a2b
Nihaal Ahmed Shibly <nshi0022@student.monash.edu>
2025-04-08 18:43:45 +1000

    Changes made for render deployment

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_cors import CORS
 from routes.location_routes import location_bp
 
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+
 
 
 app.register_blueprint(location_bp) 
@@ -15,6 +17,5 @@ def home():
 
 
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
