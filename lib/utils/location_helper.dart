f569b772efa13fa3883786fd604d85fd5ad23a4c
Nihaal Ahmed Shibly <nihaalahmed@Mac.lan>
2025-04-27 12:48:52 +0530

    added location detection functionality on app startup

---
+++ lib/utils/location_helper.dart
@@ -0,0 +1,36 @@
+import 'package:geolocator/geolocator.dart';
+
+class LocationHelper {
+  // Call this function whenever you need the current location
+  static Future<Position?> getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled, return null
+      return null;
+    }
+
+    // Check permission
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, return null
+        return null;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, return null
+      return null;
+    }
+
+    // Get the current position
+    return await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+  }
+}
