f15afc86a2095d6314526268013c0f4342efad4f
Nihaal Ahmed Shibly <nihaalahmed@dyn-118-138-31-166.its.monash.edu.au>
2025-05-16 19:21:08 +1000

    Vote Page code fix for showing markers

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,97 +1,97 @@
-import 'package:flutter/material.dart';
-import 'map_home_page.dart';
-import 'find_toilet_page.dart';
-import 'upload_page.dart';
-import 'share_page.dart';
-import 'vote_page.dart';
-import '../widgets/tutorial_overlay.dart';
+// import 'package:flutter/material.dart';
+// import 'map_home_page.dart';
+// import 'find_toilet_page.dart';
+// import 'upload_page.dart';
+// import 'share_page.dart';
+// import 'vote_page.dart';
+// import '../widgets/tutorial_overlay.dart';
 
-class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
+// class MainScreen extends StatefulWidget {
+//   const MainScreen({super.key});
 
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
+//   @override
+//   State<MainScreen> createState() => _MainScreenState();
+// }
 
-class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
+// class _MainScreenState extends State<MainScreen> {
+//   int _selectedIndex = 0;
 
-  final List<Widget> _screens = [
-    const MapHomePage(),
-    const FindToiletPage(),
-    const UploadPage(venueData: {}),
-    const SharePage(),
-    const VotePage(),
-  ];
+//   final List<Widget> _screens = [
+//     const MapHomePage(),
+//     const FindToiletPage(),
+//     const UploadPage(venueData: {}),
+//     const SharePage(),
+//     const VotePage(),
+//   ];
 
-  @override
-  void initState() {
-    super.initState();
-    // Reset tutorial state when the app starts
-    TutorialOverlay.resetTutorialState();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     // Reset tutorial state when the app starts
+//     TutorialOverlay.resetTutorialState();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: TutorialOverlay(
-        child: Scaffold(
-          body: _screens[_selectedIndex],
-          bottomNavigationBar: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  blurRadius: 8,
-                  offset: const Offset(0, -2),
-                ),
-              ],
-              borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
-            ),
-            child: ClipRRect(
-              borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
-              child: BottomNavigationBar(
-                type: BottomNavigationBarType.fixed,
-                currentIndex: _selectedIndex,
-                onTap: (index) {
-                  setState(() {
-                    _selectedIndex = index;
-                  });
-                },
-                backgroundColor: Colors.white,
-                selectedItemColor: const Color(0xFF007AFF), // iOS blue
-                unselectedItemColor: const Color(0xFF8E8E93), // iOS gray
-                selectedLabelStyle: const TextStyle(
-                  fontWeight: FontWeight.w600, // semibold
-                ),
-                items: const [
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.home),
-                    label: 'Home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.search),
-                    label: 'Find Toilet',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.upload),
-                    label: 'Upload',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.share),
-                    label: 'Share',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.thumbs_up_down),
-                    label: 'Vote',
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-} 
+//   @override
+//   Widget build(BuildContext context) {
+//     return Material(
+//       child: TutorialOverlay(
+//         child: Scaffold(
+//           body: _screens[_selectedIndex],
+//           bottomNavigationBar: Container(
+//             decoration: BoxDecoration(
+//               color: Colors.white,
+//               boxShadow: [
+//                 BoxShadow(
+//                   color: Colors.black.withOpacity(0.1),
+//                   blurRadius: 8,
+//                   offset: const Offset(0, -2),
+//                 ),
+//               ],
+//               borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
+//             ),
+//             child: ClipRRect(
+//               borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
+//               child: BottomNavigationBar(
+//                 type: BottomNavigationBarType.fixed,
+//                 currentIndex: _selectedIndex,
+//                 onTap: (index) {
+//                   setState(() {
+//                     _selectedIndex = index;
+//                   });
+//                 },
+//                 backgroundColor: Colors.white,
+//                 selectedItemColor: const Color(0xFF007AFF), // iOS blue
+//                 unselectedItemColor: const Color(0xFF8E8E93), // iOS gray
+//                 selectedLabelStyle: const TextStyle(
+//                   fontWeight: FontWeight.w600, // semibold
+//                 ),
+//                 items: const [
+//                   BottomNavigationBarItem(
+//                     icon: Icon(Icons.home),
+//                     label: 'Home',
+//                   ),
+//                   BottomNavigationBarItem(
+//                     icon: Icon(Icons.search),
+//                     label: 'Find Toilet',
+//                   ),
+//                   BottomNavigationBarItem(
+//                     icon: Icon(Icons.upload),
+//                     label: 'Upload',
+//                   ),
+//                   BottomNavigationBarItem(
+//                     icon: Icon(Icons.share),
+//                     label: 'Share',
+//                   ),
+//                   BottomNavigationBarItem(
+//                     icon: Icon(Icons.thumbs_up_down),
+//                     label: 'Vote',
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// } 
